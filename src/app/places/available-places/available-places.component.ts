159fa8250939aba5a638080fbe2a0c5c5ebfc3b5
Šárka Flídrová <sarka.flidrova@email.cz>
2025-08-15 15:32:16 +0200

    transforming and using the response data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -3,6 +3,7 @@ import { Place } from "../place.model";
 import { PlacesComponent } from "../places.component";
 import { PlacesContainerComponent } from "../places-container/places-container.component";
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs";
 
 @Component({
   selector: "app-available-places",
@@ -19,9 +20,10 @@ export class AvailablePlacesComponent implements OnInit {
   ngOnInit() {
     const subscription = this.httpClient
       .get<{ places: Place[] }>("http://localhost:3000/places")
+      .pipe(map((resData) => resData.places))
       .subscribe({
-        next: (resData) => {
-          console.log(resData.places);
+        next: (places) => {
+          this.places.set(places);
         },
       });
 
