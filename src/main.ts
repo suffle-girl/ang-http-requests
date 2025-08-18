03bf6c87fc743af07c9a9d59d9916f7ce8d6f915
Šárka Flídrová <sarka.flidrova@email.cz>
2025-08-18 16:56:21 +0200

    introducing http response interceptors - as opposed to request interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
   withInterceptors,
 } from "@angular/common/http";
+import { tap } from "rxjs";
 
 function loggingInterceptor(
   request: HttpRequest<unknown>,
@@ -17,7 +19,17 @@ function loggingInterceptor(
   console.log("[Outgoing request]");
   console.log(request);
   // return next(req);
-  return next(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log("[Incoming response]");
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, {
