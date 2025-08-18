29bb6001b36d2739e7aa04444c5193ac4ee8791c
Šárka Flídrová <sarka.flidrova@email.cz>
2025-08-18 12:36:47 +0200

    sending delete http requests

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -2,6 +2,7 @@ import { Component, DestroyRef, inject, signal } from "@angular/core";
 import { PlacesContainerComponent } from "../places-container/places-container.component";
 import { PlacesComponent } from "../places.component";
 import { PlacesService } from "../places.service";
+import { Place } from "../place.model";
 
 @Component({
   selector: "app-user-places",
@@ -27,6 +28,14 @@ export class UserPlacesComponent {
         this.isFetching.set(false);
       },
     });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
